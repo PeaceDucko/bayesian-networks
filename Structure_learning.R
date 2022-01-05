240385947e05338177c9afa6c4945ba8105faebe
Mika van Emmerloot <mika.vanemmerloot@student.ru.nl>
2022-01-05 15:15:32 +0100

    Add more structure learning results

--- Structure_learning.R
+++ Structure_learning.R
@@ -14,14 +14,28 @@ net <- pc.stable(d)
 net
 plot(net)
 
+# pc.stable returns a CPDAG, so use cextend to get a DAG
+cnet <- cextend(net)
+cnet
+plot(cnet)
+
 # Score-based Hill-Climbing
 net2 <- hc(d)
 net2
 plot(net2)
 
 # Hybrid PC + HC
-net3 <- h2pc(d)
+net3 <- h2pc(d) # MMHC and RSMAX2 also work
 net3
 plot(net3)
 
-score(net2,d,type="bic") # Find fix for this, only works with discrete data
+# TABU
+net4 <- tabu(d)
+net4
+plot(net4)
+
+type <- "bge"
+score(cnet,d,type=type)
+score(net2,d,type=type)
+score(net3,d,type=type)
+score(net4,d,type=type)
